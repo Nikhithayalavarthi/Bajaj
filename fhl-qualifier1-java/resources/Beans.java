f1c9ac598ffd1a4f64e68249dca2a0e66b9052f6
LAKSHMI NIKHITHA YALAVARTHI <nikhitha.22bce20311@vitapstudent.ac.in>
2025-08-29 11:14:39 +0530

    Add files via upload

---
+++ fhl-qualifier1-java/resources/Beans.java
@@ -0,0 +1,10 @@
+package com.example.bfhl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Beans {
+  @Bean RestTemplate restTemplate() { return new RestTemplate(); }
+}
